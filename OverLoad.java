edea37ff7426506177e6b77681bd4be0ed467e72
Ibogr <107372331+Ibogr@users.noreply.github.com>
2025-01-27 14:57:33 +0000

    Update OverLoad.java

--- OverLoad.java
+++ OverLoad.java
@@ -2,12 +2,16 @@ package udemyTim;
 
 public class OverLoad {
 	public static void main(String[] args) {
+		// main class where all the written codes runs
 		System.out.println(convertToCantimeters(2));
 		System.out.println(convertToCantimeters(5,5));
 	}
 	public static double convertToCantimeters(int heightInch) {
+		// this medhot takes 1 integer parameter.
 		double cantimeter= 2.54*heightInch;
+		// convert inch to cantimeter.
 		return cantimeter;
+		// return cantimeter
 	}
 	public static double convertToCantimeters(int feet, int inch) {
 		return (convertToCantimeters((feet*12)+inch));

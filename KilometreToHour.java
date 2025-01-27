51af36f57c9f19e123e646b339e024b8ace1bdcc
Ibogr <107372331+Ibogr@users.noreply.github.com>
2025-01-27 15:07:02 +0000

    Update KilometreToHour.java

--- KilometreToHour.java
+++ KilometreToHour.java
@@ -1,28 +1,35 @@
 package udemyTim;
 
-import org.w3c.dom.ls.LSOutput;
-
 public class KilometreToHour {
 
 	public static void main(String[] arg) {
 
 		System.out.println(toMilesPerHour(3));
+		// calling method
 
 	}
 
 	public static double toMilesPerHour(double kilomettersPerHour) {
+		//method to convert kilometres to miles.
 
 		double perKilomToMiles = 0.621371192;
+		// variable to save how a kilometer per miles.
 		double miles = 0;
+		// variable to save the miles as datatype double 
 
 		if (kilomettersPerHour < 0) {
+			// this if block returns -1 when given attribute is lower than 0.
 			return -1;
+			
 		} else {
 			miles = kilomettersPerHour * perKilomToMiles;
+			// to save the result to miles variable, after computing 
 
 		}
 		System.out.println("with double datatype: " + miles);
+		// always prints whole number of compute with fraction.
 		return Math.round(miles);
+		// return whole number of result by using math class, round method.
 
 	}
 }
